305b9bd72c3ce7a15304e07d5e652af1c38ea849
Max Leiter <maxwell.leiter@gmail.com>
2019-09-23 14:16:38 -0700

    Updates

---
+++ precache-manifest.b4a98658c03089c8fd1084f126de4573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2dacdbafb861a7a968b",
+    "url": "/cs109-interpreter/static/js/runtime~main.6693abf8.js"
+  },
+  {
+    "revision": "1493c87f6c3128264384",
+    "url": "/cs109-interpreter/static/js/main.3b2e03e2.chunk.js"
+  },
+  {
+    "revision": "00ca9b259fbd40c6d902",
+    "url": "/cs109-interpreter/static/js/2.992f3dd8.chunk.js"
+  },
+  {
+    "revision": "1493c87f6c3128264384",
+    "url": "/cs109-interpreter/static/css/main.68ffe2b0.chunk.css"
+  },
+  {
+    "revision": "00ca9b259fbd40c6d902",
+    "url": "/cs109-interpreter/static/css/2.f975ae79.chunk.css"
+  },
+  {
+    "revision": "a0579def84d259e22b6f5aa2d58720be",
+    "url": "/cs109-interpreter/index.html"
+  }
+];
